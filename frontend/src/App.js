4c670e56e93957bd1e3772cdf0766efb8d0f2895
Sravyab <sravyaboinpally@gmail.com>
2026-05-08 15:33:09 -0400

    Initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '100px' }}>
+      <h1>My Docker Website</h1>
+      <p>Built using React + Docker</p>
+    </div>
+  );
+}
+
+export default App;
